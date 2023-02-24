aa38487533001b575c2cd93770fd03ede7ba890d
unknown <G00401878@atu.ie>
2023-02-24 12:15:15 +0000

    Lab4 complete

---
+++ src/app/a/a.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.css']
+})
+export class AComponent {
+ test:string = "Pancakes";
+ numOfPancakes:number = 3;
+}
